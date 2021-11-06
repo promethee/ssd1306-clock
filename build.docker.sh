a65dbd588ee5a8c33b7d68a9ffa488beed6b1548
promethee <lunette.francois@gmail.com>
2021-11-06 18:11:23 +0100

    Update build.docker.sh

--- build.docker.sh
+++ build.docker.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-docker build -t $USER/$(basename "$PWD"):$(arch) .
+NAME=n$(md5sum Dockerfile | cut -c-4)
+echo "using building context:" $NAME
+docker buildx create --use --name $NAME
+docker buildx build --push --platform linux/arm/v6,linux/arm/v7 --tag $USER/$(basename "$PWD"):latest .
+docker buildx rm $NAME

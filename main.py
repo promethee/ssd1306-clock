719e803493b45028decee178ce06bc9533f1fb8e
promethee <lunette.francois@gmail.com>
2021-11-17 16:42:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def show_emoji():
   clear()
   draw.rectangle([0, 0, WIDTH, HEIGHT], fill=0)
   font_smiley = ImageFont.truetype('./CODE2000.TTF', 32)
-  draw.text((0, -4), "¯\_(ツ)_/¯", font=font, fill=1)
+  draw.text((0, -4), "¯\_(ツ)_/¯", font=font_smiley, fill=1)
   rotated_image = im.transpose(Image.ROTATE_180) if ROTATE else im
   display.image(rotated_image)
   display.show()

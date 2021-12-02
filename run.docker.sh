2fcca78fe59f9d03969df5c4cfcc776231ff3247
promethee <lunette.francois@gmail.com>
2021-12-02 19:21:59 +0100

    Update run.docker.sh

--- run.docker.sh
+++ run.docker.sh
@@ -3,4 +3,4 @@
 # -v /etc/localtime:/etc/localtime : allow to have the same timezone setting inside the container
 # --device /dev/i2c-1:/dev/i2c-1 & --device /dev/spidev0.0:/dev/spidev0.0 : required to access the PiOLED
 # --restart always : required to prevent the clock from stopping
-docker run -d -e ROTATE=False --privileged -v /etc/localtime:/etc/localtime --device /dev/i2c-1:/dev/i2c-1 --device /dev/spidev0.0:/dev/spidev0.0 --restart always --name clock promethee/ssd1306-clock:latest
+docker run -d -e ROTATE=False --privileged -v /etc/localtime:/etc/localtime --device /dev/i2c-1:/dev/i2c-1 --device /dev/spidev0.0:/dev/spidev0.0 --restart always --name $(basename "$PWD") $USER/$(basename "$PWD"):latest

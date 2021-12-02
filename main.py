19575bcc6a9eba7c7852c64c792b5ae58fd46526
promethee <lunette.francois@gmail.com>
2021-12-02 19:18:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ i2c = busio.I2C(SCL, SDA)
 WIDTH = os.environ.get('WIDTH', 128)
 HEIGHT = os.environ.get('HEIGHT', 32)
 ROTATE = os.environ.get('ROTATE', False)
-FONT_SIZE = os.environ.get('FONT_SIZE', 38)
+FONT_SIZE = int(os.environ.get('FONT_SIZE', 38))
 OFFSET_X = os.environ.get('OFFSET_X', 0)
-OFFSET_Y = os.environ.get('OFFSET_Y', -8)
+OFFSET_Y = os.environ.get('OFFSET_Y', -4)
 SHOW_DATE = "DATE"
 SHOW_TIME = "TIME"
 SHOW = os.environ.get('SHOW', SHOW_DATE)
@@ -26,60 +26,45 @@ WAIT = dict({
   "TIME": os.environ.get('WAIT_TIME', 5),
 })
 
-
-display = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c)
+fonts = dict({
+  "emoji": ImageFont.truetype('./CODE2000.TTF', 24),
+  "credits": ImageFont.truetype(RobotoMedium, 24),
+  "DATE": ImageFont.truetype(RobotoMedium, FONT_SIZE),
+  "TIME": ImageFont.truetype(RobotoMedium, FONT_SIZE),
+})
 
 def get_text():
   global SHOW
-  return datetime.datetime.now().astimezone(None).strftime(
-    "%H:%M" if SHOW == SHOW_TIME else "%a %d"
-  )
-
-font = ImageFont.truetype(RobotoMedium, FONT_SIZE)
-(TEXT_WIDTH, TEXT_HEIGHT) = font.getsize(get_text())
-
-im = Image.new("1", (WIDTH, HEIGHT), 0)
-draw = ImageDraw.Draw(im)
+  display_format = "%H:%M" if SHOW == SHOW_TIME else "%a %d"
+  return datetime.datetime.now().astimezone(None).strftime(display_format)
 
 def clear():
   display.fill(0)
   display.image(im)
 
-def show_time():
+display = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c)
+
+emoji = "¯\_(ツ)_/¯"
+platform = "@github"
+author = "promethee"
+
+im = Image.new("1", (WIDTH, HEIGHT), 0)
+draw = ImageDraw.Draw(im)
+
+def show(text, font):
   clear()
-  text = get_text()
+  size = font.getsize(text)
   draw.rectangle([0, 0, WIDTH, HEIGHT], fill=0)
-  draw.text((OFFSET_X, OFFSET_Y), text, align='center', font=font, fill=1)
+  draw.text((OFFSET_X + ((WIDTH/2)-(size[0]/2)), OFFSET_Y), text, align='center', font=font, fill=1)
   rotated_image = im.transpose(Image.ROTATE_180) if ROTATE else im
   display.image(rotated_image)
   display.show()
 
-def show_emoji():
-  clear()
-  draw.rectangle([0, 0, WIDTH, HEIGHT], fill=0)
-  font_smiley = ImageFont.truetype('./CODE2000.TTF', 26)
-  draw.text((0, -4), "¯\_(ツ)_/¯", font=font_smiley, fill=1)
-  rotated_image = im.transpose(Image.ROTATE_180) if ROTATE else im
-  display.image(rotated_image)
-  display.show()
-
-def show_credits(text):
-  clear()
-  draw.rectangle([0, 0, WIDTH, HEIGHT], fill=0)
-  font = ImageFont.truetype(RobotoMedium, 24)
-  draw.text((0, -4), text, font=font, fill=1)
-  rotated_image = im.transpose(Image.ROTATE_180) if ROTATE else im
-  display.image(rotated_image)
-  display.show()
-
-show_emoji()
-time.sleep(1.5)
-show_credits("promethee")
-time.sleep(1.5)
-show_credits("@github")
-time.sleep(1.5)
+for text,font_key in [(emoji, "emoji"), (author, "credits"), (platform, "credits")]:
+  show(text, fonts[font_key])
+  time.sleep(1)
 
 while True:
   SHOW = SHOW_TIME if SHOW == SHOW_DATE else SHOW_DATE
-  show_time()
+  show(get_text(), fonts[SHOW])
   time.sleep(WAIT[SHOW])
